a15701248720cd39efc42993f0aa6003a79d16fe
afdon <53676753+afdon@users.noreply.github.com>
2023-10-03 12:19:25 -0700

    bugfix error

--- 3-calculator/script.js
+++ 3-calculator/script.js
@@ -13,11 +13,16 @@ const handleClick = (event) => {
     store += event.target.innerHTML;
     console.log(store);
     console.log(typeof store)
+    console.log(store[-1])
+
+    // if (store.slice(-1) === "=") {
+    //     store = store.slice(0, -1);
+    //     store = eval(store)
+    // }
 
     if (store.slice(-1) === "=") {
-        // evaluate the statement
-        store.slice(0, -1)
-        eval(store) // WHY IS THIS NOT WORKINGGGGGG
+        // evaluate the statement and store the result
+        store = eval(store.slice(0, -1)); // WHY DOESN'T THIS WORK
     }
 
     if (store.slice(-1) === "C") {
