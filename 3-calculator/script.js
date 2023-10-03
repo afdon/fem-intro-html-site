aaafec90a2c2ff10b55acf86f19e33ab6a0b3465
afdon <53676753+afdon@users.noreply.github.com>
2023-10-03 11:45:26 -0700

    removed parseInt

--- 3-calculator/script.js
+++ 3-calculator/script.js
@@ -5,13 +5,15 @@ let store = 0
 const handleClick = (event) => {
     console.log(event)
     // if the button's value is a number, store it.
-    if (typeof parseInt(event.target.innerHTML) === "number") {
-        store += event.target.innerHTML;
-        console.log(store);
-    }
+    // if (typeof parseInt(event.target.innerHTML) === "number") {
+    //     store += event.target.innerHTML;
+    //     console.log(store);
+    // }
 
-    // store += event.target.innerHTML;
-    // console.log(store);
+    store += event.target.innerHTML;
+    console.log(store);
+
+
 
     // reflect it in the display.
     document.getElementById("results-display").innerHTML = store;
