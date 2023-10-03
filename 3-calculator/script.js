3486b5eb45d55cbb6c2df3d5341f70e1c805c5c1
afdon <53676753+afdon@users.noreply.github.com>
2023-10-03 11:44:20 -0700

    display stored values

--- 3-calculator/script.js
+++ 3-calculator/script.js
@@ -1,9 +1,23 @@
+
+
+let store = 0
+
 const handleClick = (event) => {
     console.log(event)
-    document.getElementById("results-display").innerHTML="Hello Jarvan";
+    // if the button's value is a number, store it.
+    if (typeof parseInt(event.target.innerHTML) === "number") {
+        store += event.target.innerHTML;
+        console.log(store);
+    }
+
+    // store += event.target.innerHTML;
+    // console.log(store);
+
+    // reflect it in the display.
+    document.getElementById("results-display").innerHTML = store;
 }
 
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach(btn => {
     btn.addEventListener('click', handleClick);
