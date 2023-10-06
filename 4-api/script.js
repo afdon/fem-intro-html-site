2cab5d11c2a7f38e209f11bcb7c195a153e7d154
afdon <53676753+afdon@users.noreply.github.com>
2023-10-06 11:50:32 -0700

    begin apis

---
+++ 4-api/script.js
@@ -0,0 +1,24 @@
+const DOG_URL="https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.getElementById("dog-target");
+
+function addNewDoggo() {
+    const promise = fetch(DOG_URL);
+    promise
+        .then(function (response) {
+            const processingPromise = response.text();
+            return processingPromise;
+        })
+        .then(function (processedResponse) {
+            const dogObject = JSON.parse(processedResponse);
+            const img = document.createElement("img");
+            img.src = dogObject.message;
+            img.alt = "Cute doggo";
+            doggos.appendChild(img);
+        }).catch(function(error) {
+            // handle the error
+        })
+}
+
+document.getElementById("dog-btn").addEventListener("click", addNewDoggo);
+ 
