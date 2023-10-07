85b76440b09ee9afaea8a8020bdf77baef818021
afdon <53676753+afdon@users.noreply.github.com>
2023-10-07 10:58:54 -0700

    installed popmotion

--- 6-animations/dist/index.afac068d.js
+++ 6-animations/dist/index.afac068d.js
@@ -1,16 +1,15 @@
-const popmotion = requre('popmotion')
-
+const popmotion = requre("popmotion");
 const ball = document.querySelector(".ball");
-
 popmotion.animate({
     from: "0px",
     to: "100px",
     repeat: Infinity,
     repeatType: "mirror",
     type: "spring",
-    onUpdate(update) {
+    onUpdate (update) {
         // how on earth does popmotion have access to ball.style
         ball.style.left = update;
     }
 });
 
+//# sourceMappingURL=index.afac068d.js.map
